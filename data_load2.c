d30a31f0d6081672a23439d62222a57d56e1a7c1
vishal <subhavishal@yahoo.co.in>
2024-04-10 23:48:15 -0400

    G and G_X run now

--- data_load2.c
+++ data_load2.c
@@ -29,24 +29,28 @@ int main(){
 	char filename2[] = "./data/Case 14 Generator.csv";
 	char filename3[] = "./data/Case 14 Generator Cost.csv";
 	char filename4[] = "./data/Case 14 Branch.csv";
-	//char filename5[] = "Case 14 REG.csv";
+	char filename5[] = "./data/Case 14 REG one scen.csv";
   
 	// declare temp vectors for Bus Data
 	std::vector<double> VoltAng;
 	std::vector<double> Volt;
 	std::vector<double> VoltMax;
 	std::vector<double> VoltMin;
+	vector<double> RealPowerDemand, ReactPowerDemand;
 	// following code is to read Bus data
 	{
 		io::CSVReader<7> in(filename1);
 		in.read_header(io::ignore_extra_column, "Bus ID", "Real Power Demand", "React Power Demand", "Volt Magnitude", "Max Volt Magnitude", "Min Volt Magnitude", "Volt Angle");
 		double Bus; double Pd; double Qd; double V; double Vmax; double Vmin; double Vang;
+		
 		while(in.read_row(Bus, Pd, Qd, V, Vmax, Vmin, Vang)){
 			// get Voltages and Voltage Angles
 			VoltAng.insert(VoltAng.end(), Vang);
 			Volt.insert(Volt.end(), V);
 			VoltMax.insert(VoltMax.end(), Vmax);
 			VoltMin.insert(VoltMin.end(), Vmin);
+			RealPowerDemand.push_back(Pd);
+			ReactPowerDemand.push_back(Qd);
 		}
 	}
   
@@ -57,6 +61,7 @@ int main(){
 	std::vector<double> RealPowerMin;
 	std::vector<double> ReactPowerMax;
 	std::vector<double> ReactPowerMin;
+	vector<double> BusID_Gen;
   	// following code is to read Generator data
 	{
 		io::CSVReader<7> in(filename2);
@@ -70,6 +75,7 @@ int main(){
 			RealPowerMin.insert(RealPowerMin.end(), Pmin);
 			ReactPowerMax.insert(ReactPowerMax.end(), Qmax);
 			ReactPowerMin.insert(ReactPowerMin.end(), Qmin);
+			BusID_Gen.push_back(Bus);
 		}
 	}
 	
@@ -91,22 +97,38 @@ int main(){
 	}
 	
 	// declare temp vectors for Branch Data
-	std::vector<double> FromBus;
-	std::vector<double> ToBus;
-	std::vector<double> VoltAngMax;
-	std::vector<double> VoltAngMin;
-  	// following code is to read Generator Cost data
+	vector<double> VoltAngMax, VoltAngMin, Gvec, Bvec;
+	vector<double> FromBus, ToBus;
+  	// following code is to read Branch Data
 	{
 		io::CSVReader<6> in(filename4);
 		in.read_header(io::ignore_extra_column, "From Bus", "To Bus", "Max Angle Difference", "Min Angle Difference", "G" , "B");
-		double FBus; double TBus; double MaxAng; double MinAng; double G; double B;
-		while(in.read_row(FBus, TBus, MaxAng, MinAng, G, B)){
+		int FBus, TBus; 
+		double MaxAng, MinAng, Gij, Bij;
+		while(in.read_row(FBus, TBus, MaxAng, MinAng, Gij, Bij)){
 			// get From Bus, To Bus, and Voltage Angle Max and Min values
 			FromBus.insert(FromBus.end(), FBus);
 			ToBus.insert(ToBus.end(), TBus);
 			VoltAngMax.insert(VoltAngMax.end(), MaxAng);
 			VoltAngMin.insert(VoltAngMin.end(), MinAng);
+			Gvec.push_back(Gij);
+			Bvec.push_back(Bij);
 		}
+	}
+
+	vector<double> Real_P_REG, React_P_REG;
+	vector<double> BusID_REG;
+	{
+		io::CSVReader<7> in(filename5);
+		in.read_header(io::ignore_extra_column, "Bus ID", "Real Power Output", "React Power Output", "Max Real Power Output", "Min Real Power Output", "Max React Power Output", "Min React Power Output");
+		
+		double REG_ID, n,o,m,l,	P, Q; 
+		while(in.read_row(REG_ID, n,o,m,l,	P, Q)){
+			BusID_REG.push_back(REG_ID); 
+			Real_P_REG.push_back(P); 
+			React_P_REG.push_back(Q); 
+
+		}	
 	}
     
     /*
@@ -150,19 +172,19 @@ int main(){
 	printf("Number of Equations: %d, Number of Variables: %d\n", sizeY, sizeX);
 	
 	// Construct Problem
-	f(X, a, b, c, Cost);
+	// f(X, a, b, c, Cost);
 //	for (Real i: fX)
 //		std::cout << i << ' ';
 //	printf("\n");
 
-	H(X, RealPowerMax, RealPowerMin, ReactPowerMax, ReactPowerMin, VoltMax, VoltMin, FromBus, ToBus, VoltAngMax, VoltAngMin, HX);
+	// H(X, RealPowerMax, RealPowerMin, ReactPowerMax, ReactPowerMin, VoltMax, VoltMin, FromBus, ToBus, VoltAngMax, VoltAngMin, HX);
 //	for (Real i: HX)
 //		std::cout << i << std::endl;
 //	printf("\n");
 
-	for (int i=0; i < sizeY; i++) {
-		Z[i] = HX[i]*-1.0;
-	}
+	// for (int i=0; i < sizeY; i++) {
+	// 	Z[i] = HX[i]*-1.0;
+	// }
 //	for (Real i: Z)
 //		std::cout << i << ' ';
 //	printf("\n");
@@ -176,7 +198,7 @@ int main(){
 		}
 		std::cout << std::endl;
 	}*/
-	
+
 	/************************************
 	 Testing for f_X
 	*************************************/
@@ -190,6 +212,32 @@ int main(){
 	// 		cout<<j << ' ';
 	// 	cout<<"\n";
 	// }
+
+	/************************************
+	 Testing for G, G_X
+	*************************************/
+
+
+	int Nb = FromBus.size();
+	Real G_val[2*Nb];
+	G_func( X, BusID_Gen, BusID_REG, FromBus, ToBus, RealPowerDemand, ReactPowerDemand, Gvec, Bvec, Real_P_REG, React_P_REG, G_val);
+	vector<vector<Real>> G_X(2*Nb, vector<Real>(sizeX));
+	for(Real val: G_val)
+	{
+		cout<<val<<" ";
+	}
+	cout<<endl;
+	G_X = GX_func(X, sizeX, G_val, 2*Nb, BusID_Gen, BusID_REG, FromBus, ToBus, RealPowerDemand, ReactPowerDemand, Gvec, 
+			 Bvec, Real_P_REG, React_P_REG,G_X );
+	cout<<"First derivative"<<endl;
+	for(vector<Real> v: G_X)
+	{
+		for(Real val: v)
+			printf("%.1f ",val.value());
+		cout<<endl;
+	}
+	
+	
 }
 
 
--- define2.h
+++ define2.h
@@ -49,86 +49,111 @@ vector<vector<Real>> fX(Real* X, int XSize, Real* Y, int YSize, vector<double> a
 }
 
 
-// void G(const Real* X, vector<double> BusID_Gen, vector<double> BusID_REG, vector<int> FromBus, vector<int> ToBus,
-// 				 double* PowerReg, vector<double> RealPowerDemand, vector<double> ReactPowerDemand, vector<double> G,
-// 				 vector<double> B, vector<double> Real_P_G, vector<double> React_P_G, vector<double> Real_P_REG,
-// 				 vector<double> React_P_REG, Real* result) {
-// 	// Variable Definitions
-// 	Real temp = 0;
-// 	int Xplace = 0, counter = 0;
-// 	//vector<double> result;
-// 	vector<vector<double> > G_map, B_map, theta;
-// 	int Nb = RealPowerDemand.size();
+void G_func(const Real* X, vector<double> BusID_Gen, vector<double> BusID_REG, vector<double> FromBus, vector<double> ToBus,
+			 vector<double> RealPowerDemand, vector<double> ReactPowerDemand, vector<double> G, vector<double> B, vector<double> Real_P_REG,
+				 vector<double> React_P_REG, Real* result) {
+	// Variable Definitions
+	Real temp = 0;
+	int Xplace = 0;
+	//vector<double> result;
+	vector<vector<Real> > G_map, B_map, theta;
+	int Nb = RealPowerDemand.size();
 	
-// 	// Fill theta, G and B values
-// 	for( int i = 0; i < Nb; i++ )
-// 	{
-// 		vector<double> v(Nb, 0.0);
-// 		G_map.push_back(v);
-// 		B_map.push_back(v);
-// 		theta.push_back(v);
-// 	}
-// 	int Nbranches = FromBus.size();
-// 	for (int i=0; i < Nbranches; i++) 
-// 	{
-// 		theta[FromBus[i]-1][ToBus[i]-1] = X[3*Nb+FromBus[i]-1] - X[3*Nb+ToBus[i]-1];
-// 		G_map[FromBus[i]-1][ToBus[i]-1] = G[i];
-// 		B_map[FromBus[i]-1][ToBus[i]-1] = B[i];
-// 	}
+	// Fill theta, G and B values
+	for( int i = 0; i < Nb; i++ )
+	{
+		vector<Real> v(Nb, 0.0);
+		G_map.push_back(v);
+		B_map.push_back(v);
+		theta.push_back(v);
+	}
+	int Nbranches = FromBus.size();
+	for (int i=0; i < Nbranches; i++) 
+	{
+		theta[FromBus[i]-1][ToBus[i]-1] = X[3*Nb+int(FromBus[i])-1] - X[3*Nb+int(ToBus[i])-1];
+		G_map[FromBus[i]-1][ToBus[i]-1] = G[i];
+		B_map[FromBus[i]-1][ToBus[i]-1] = B[i];
+	}
 	
-// 	// Fill Power terms
-// 	vector<double> P_G(Nb, 0.0), P_R(Nb, 0.0), Q_G(Nb, 0.0), Q_R(Nb, 0.0);
-// 	int NG = BusID_Gen.size(), NR = BusID_REG.size();
-// 	for( int i = 0; i < Nb; i++ )
-// 	{
-// 		for( int j = 0; j < NG; j++ )
-// 		{
-// 			if( i + 1 == BusID_Gen[j] )
-// 			{	
-// 				P_G[i] = X[j];
-// 				Q_G[i] = X[j + NG];
-// 				continue;
-// 			}
-// 		}
-// 		for( int j = 0; j < NR; j++ )
-// 		{
-// 			if( i + 1 == BusID_REG[j] )
-// 			{	
-// 				P_R[i] = Real_P_REG[j];
-// 				Q_R[i] = React_P_REG[j];
-// 				continue;
-// 			}
-// 		}
-// 	}
+	// Fill Power terms
+	vector<Real> P_G(Nb, 0.0), P_R(Nb, 0.0), Q_G(Nb, 0.0), Q_R(Nb, 0.0);
+	int NG = BusID_Gen.size(), NR = BusID_REG.size();
+	for( int i = 0; i < Nb; i++ )
+	{
+		for( int j = 0; j < NG; j++ )
+		{
+			if( i + 1 == BusID_Gen[j] )
+			{	
+				P_G[i] = X[j];
+				Q_G[i] = X[j + NG];
+				continue;
+			}
+		}
+		for( int j = 0; j < NR; j++ )
+		{
+			if( i + 1 == BusID_REG[j] )
+			{	
+				P_R[i] = Real_P_REG[j];
+				Q_R[i] = React_P_REG[j];
+				continue;
+			}
+		}
+	}
 
-// 	double temp = 0, temp2 = 0;
+	Real temp2 = 0;
+	int counter = 0;
 	
-// 	// Real Power Balances
-// 	for(int i = 0; i < Nb; i++ )
-// 	{
-// 		temp = P_G[i] + P_R[i] - RealPowerDemand[i];
+	// Real Power Balances
+	for(int i = 0; i < Nb; i++ )
+	{
+		temp = P_G[i] + P_R[i] - RealPowerDemand[i];
 		
-// 		temp2 = 0;
-// 		for(int j = 0; j< Nb; j++)
-// 		{
-// 			temp2 += X[2*NG + j]*( G_map[i][j]*cos(theta[i][j]) + B_map[i][j]*sin(theta[i][j]) );
-// 		}
-// 		result[i] = temp - temp2*X[2*NG + i];
-// 	}
+		temp2 = 0;
+		for(int j = 0; j< Nb; j++)
+		{
+			temp2 += X[2*NG + j]*( G_map[i][j]*cos(theta[i][j]) + B_map[i][j]*sin(theta[i][j]) );
+		}
+		result[counter++] = temp - temp2*X[2*NG + i];
+	}
 	
-// 	// Reactive Power Balances
-// 	for(int i = 0; i < Nb; i++ )
-// 	{
-// 		temp = Q_G[NG + i] + Q_R[NG + i] - ReactPowerDemand[NG + i];
+	// Reactive Power Balances
+	for(int i = 0; i < Nb; i++ )
+	{
+		temp = Q_G[NG + i] + Q_R[NG + i] - ReactPowerDemand[NG + i];
 		
-// 		temp2 = 0;
-// 		for(int j = 0; j< Nb; j++)
-// 		{
-// 			temp2 += X[2*NG + j]*( G_map[i][j]*sin(theta[i][j]) - B_map[i][j]*cos(theta[i][j]) );
-// 		}
-// 		result[i] = temp - temp2*X[2*NG + i];
-// 	}
-// }
+		temp2 = 0;
+		for(int j = 0; j< Nb; j++)
+		{
+			temp2 += X[2*NG + j]*( G_map[i][j]*sin(theta[i][j]) - B_map[i][j]*cos(theta[i][j]) );
+		}
+		result[counter++] = temp - temp2*X[2*NG + i];
+	}
+}
+
+vector<vector<Real>> GX_func(Real* X, int XSize, Real* Y, int YSize, vector<double> BusID_Gen, vector<double> BusID_REG, vector<double> FromBus, vector<double> ToBus,
+			 vector<double> RealPowerDemand, vector<double> ReactPowerDemand, vector<double> Gvec, vector<double> Bvec, vector<double> Real_P_REG,
+				 vector<double> React_P_REG, vector<vector<Real>> result)
+{
+	//printf("Number of Equations: %d, Number of Variables: %d\n", YSize, XSize);
+	for(int i = 0; i < XSize; ++i) {
+		// Step 1: Set tangent seeding
+		X[i].gradient() = 1.0;
+		// Step 2: Evaluate function
+		G_func(X, BusID_Gen, BusID_REG, FromBus, ToBus, RealPowerDemand, ReactPowerDemand, Gvec, Bvec, Real_P_REG, React_P_REG, Y);
+		for(int j = 0; j < YSize; ++j){
+
+			// Step 3: Access gradients
+			result[j][i] = Y[j].getGradient();
+
+		}
+		// Step 4: Reset tangent seeding
+		X[i].gradient() = 0.0;
+		//printf("%d ", j);
+	}
+	//fflush(stdout);
+	
+	return result;
+}
 
 
 
