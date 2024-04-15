ea5a358240e1535e5161743307cc958dae3923df
vishal <subhavishal@yahoo.co.in>
2024-04-15 16:08:21 -0400

    hessian works for an example problem

--- higher_order_deriv_trials/hessian-trial.cpp
+++ higher_order_deriv_trials/hessian-trial.cpp
@@ -1,25 +1,43 @@
 #include<bits/stdc++.h>
 #include<codi.hpp>
 using namespace std;
-
+#include "outputHelpers.hpp"
+ 
 using EH = codi::EvaluationHelper;
-
+// using Real = codi::RealForward;
 template<typename Real>
-void func(const Real*  x, Real* t) {
-  t[0] = x[0] * x[0] * x[0];
-  t[1] = x[0]*x[1]*30;
-  t[2] = 5*x[1]*x[1];
+void func(Real const*  x, Real* t) {
+  // t = Real();
+  t[0] = 4 * x[1] * x[0] + 2*x[1]*x[2] + x[2];
 }
-
-auto hes = EH::createHessian(3, 2);
+template<typename Real>
+void funcwrap(std::vector<Real> const &x, std::vector<Real> &y) {
+  size_t n = x.size() / 2;
+  func(&x[0], &y[0]);
+}
+auto hes = EH::createHessian(3, 3);
 int main()
 {
-  double x[] = {1,2}, y[3];
-  func(x,y);
-  for(int i = 0; i <3; ++i)
-  {
-    cout<<y[i]<<" ";
-  }
+  vector<double> x(3), y(1);
+  x[0] = 1;
+  x[1] = 2;
+  x[2] = 3;
+  funcwrap(x,y);
+  cout<<y[0];
   cout<<endl;
+  EH::evalHessian(funcwrap<EH::HessianComputationType>, x, 3, hes);
+  for(size_t j = 0; j < hes.getN(); j += 1) 
+  {
+    std::cout << "  ";
+    for(size_t k = 0; k < hes.getN(); k += 1) 
+    {
+      if(k != 0) 
+      {
+        std::cout << ", ";
+      }
+      std::cout << hes(0, j, k);
+    }
+    std::cout << "\n";
+  }
   return 0;
 }
